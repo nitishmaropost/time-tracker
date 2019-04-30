ed3e09532fb49523ff7ef17391481790f9e3cc01
maropost-neha <neha.kareer@maropost.com>
2019-04-30 15:25:32 +0530

    Home Fragment- barchart and today/yesterday date format

--- app/src/main/java/com/maropost/timetracker/pojomodels/TimeUtils.kt
+++ app/src/main/java/com/maropost/timetracker/pojomodels/TimeUtils.kt
@@ -1,7 +1,7 @@
 package com.maropost.timetracker.pojomodels
 
 class TimeUtils {
-    var hours: String= ""
+    var hours: Float= 0f
     var day: String= ""
     var date: String= ""
     var inTime: String= ""
