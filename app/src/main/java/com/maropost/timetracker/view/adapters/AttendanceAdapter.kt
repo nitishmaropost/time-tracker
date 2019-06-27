3904bdc6a43b5b2e99526af2d996c9e7dacfde92
maropost <prabhtej.singh@maropost.com>
2019-06-27 17:06:21 +0530

    Commit

--- app/src/main/java/com/maropost/timetracker/view/adapters/AttendanceAdapter.kt
+++ app/src/main/java/com/maropost/timetracker/view/adapters/AttendanceAdapter.kt
@@ -36,31 +36,32 @@ class AttendanceAdapter (private var arrayList: ArrayList<RowShifts>, val contex
     @SuppressLint("SetTextI18n", "SimpleDateFormat")
     override fun onBindViewHolder(holder: AttendanceViewHolder, position: Int) {
 
+        // Emp name
         holder.txtEmpName?.text = arrayList[position].user_data?.full_name
+
+        // Emp Code
         holder.txtEmpCode?.text = "Emp Code: " + arrayList[position].user_data?.emp_code
 
+        // Emp email
         val email = arrayList[position].user_data?.email_id
         val builder = SpannableStringBuilder()
-
         builder.append("Email: ")
-
-        val redSpannable = SpannableString(email)
-        redSpannable.setSpan(ForegroundColorSpan(Color.BLUE), 0, email!!.length, 0)
-        builder.append(redSpannable)
-        //holder.txtEmail?.text = "Email: " + arrayList[position].user_data?.email_id
+        val spannable = SpannableString(email)
+        spannable.setSpan(ForegroundColorSpan(Color.BLUE), 0, email!!.length, 0)
+        builder.append(spannable)
         holder.txtEmail?.setText(builder, TextView.BufferType.SPANNABLE)
 
-
+        // Emp position
         if(!TextUtils.isEmpty(arrayList[position].user_data?.position)) {
             holder.txtPosition?.text = arrayList[position].user_data?.position
             holder.txtPosition?.visibility = View.VISIBLE
         }else holder.txtPosition?.visibility = View.GONE
 
+        // Emp department
         if(!TextUtils.isEmpty(arrayList[position].user_data?.department)) {
-            holder.txtDepartment?.text = "Dept: "+arrayList[position].user_data?.department
+            holder.txtDepartment?.text = arrayList[position].user_data?.department
             holder.txtDepartment?.visibility = View.VISIBLE
         }else holder.txtDepartment?.visibility = View.GONE
-
     }
 }
 
